f54ffd446c52d39b2235888b455608b4ddb0a591
Dimo Grigorov <drgrigorov@gmail.com>
2017-07-21 11:09:50 +0300

    Added some basic comments for methods

--- main.cpp
+++ main.cpp
@@ -6,17 +6,25 @@
 
 
 
+//The class usses a "cache" of rows for each three level.
+//This will make it a lot more effective in terms of add/remove/isWord, but will consume more memory
 class Node
 {
 private:
 	//Did not know that those were introduced in c++11
 	static const uint8_t MAXSIZE = (('Z' - 'A') + ('z' - 'a') + 1);
+
 	typedef std::vector< Node* > Nodes;
 
+	//Holds an "array" of children
 	Nodes m_Children;
+
+	//Saves information if the node is an end of a word.
 	bool m_bIsTerminal;
 
 
+	//This is simple indexer function which returns consecutive numbers from 0 to N so we can
+	//access the "array".
 	const uint8_t Index( char cFrom ) const throw()
 	{
 		if (cFrom >= 'a')
@@ -38,6 +46,8 @@ private:
 	}
 
 public:
+	//We initialize the children with 0 (not existing at the moment)
+	//and each node starts as non-terminal
 	Node() throw()
 	:
 		m_Children( MAXSIZE, 0 ),
@@ -45,6 +55,8 @@ public:
 	{
 	}
 
+	//Helper funciton to print the index function.
+	//it is a dumb way to verify the index integrity
 	void printIndexTable() const throw()
 	{
 		for (char c = 'A'; c <= 'Z'; c++)
@@ -57,6 +69,11 @@ public:
 		}
 	}
 
+	/** inserts a word in the tree (or subtree)
+	 * @sWord - word to be inserted
+	 * 
+	 * returns true if string is exhausted
+	 */
 	bool insert( const std::string& sWord ) throw()
 	{
 		//emtpy word or end of word
@@ -82,6 +99,13 @@ public:
 		}
 	}
 
+	/** removes a word in the tree (or subtree) and
+	 * takes care for memory deletion
+	 *
+	 * @sWord - word to be removed
+	 * 
+	 * returns true if string is exhausted
+	 */
 	bool remove( const std::string& sWord ) throw()
 	{
 		//String exhausted, we need to set this node as non-terminal
@@ -107,6 +131,12 @@ public:
 		}
 	}
 
+	/** Checks if word is present in dictionary
+	 *
+	 * @sWord - word to be checked
+	 * 
+	 * returns true the word is present in the three (and terminated)
+	 */
 	bool isWord( const std::string& sWord ) const throw()
 	{
 		//emtpy word or end of word
@@ -126,6 +156,7 @@ public:
 		}
 	}
 
+	///Returns the height of the tree including root node (will be max word +1)
 	int getHeight() const throw()
 	{
 		//This method prints the height including root node.
